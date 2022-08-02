fa7e856fcea44cce1dc1b45ab2178342ebeaeb4d
Surajit Sarkar <surajitsarkar@Surajits-MBP.bbrouter>
2022-08-02 19:42:11 +0530

    Initialised and added a GET API Call

---
+++ src/test/scala/simulations/TestAPISimulation.scala
@@ -0,0 +1,22 @@
+package simulations;
+
+import io.gatling.core.scenario.Simulation;
+import io.gatling.core.Predef._;
+import io.gatling.http.Predef._;
+
+class TestAPISimulation extends Simulation{
+  //http config
+  val httpConf = http.baseUrl("https://reqres.in/")
+    .header("Accept","application/json")
+    .header("content-type","application/json")
+
+  //scenario
+  val scn = scenario("Get single user")
+    .exec(http("Get User Request")
+      .get("/api/users/2")
+      .check(status is 200)
+    )
+
+  //setup
+  setUp(scn.inject(atOnceUsers(1000))).protocols(httpConf)
+}
